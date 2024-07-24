9e66efc2fa92ccbc013ea5d729501c9ee320bf62
amphetaminefather <158784347+Silene0259@users.noreply.github.com>
2024-07-24 08:33:15 -0700

    Update storage.rs

--- src/fs/storage.rs
+++ src/fs/storage.rs
@@ -3,22 +3,67 @@ use std::{ffi::OsStr, path::{Path,PathBuf}};
 use std::fs;
 
 pub const SUMATRANAME: &str = ".sumatra";
+pub const CONFIGNAME: &str = "config";
+pub const KEYSPACE: &str = "keyspace";
 
-pub struct SumatraHomeDirectory(Path);
+/// Home Directory `$HOMEDIR/.sumatra`
+pub struct SumatraHomeDirectory(PathBuf);
+
+/// Config Location `(.sumatra/config/)`
+pub struct SumatraHomeDirectoryConfig(PathBuf);
+
+/// Keyspace Location `(.sumatra/keyspace/)``
+pub struct SumatraHomeDirectoryKeyspace(PathBuf);
+
+
+pub struct KeyspacePersonalKeys(PathBuf);
 
 impl SumatraHomeDirectory {
-    pub fn init() {
+    pub fn init_home() -> SumatraHomeDirectory {
         let home_dir = home_dir().expect("Failed To Get Home Dir");
         let mut current_path = home_dir;
 
 
         current_path.push(Path::new(SUMATRANAME));
 
-        fs::create_dir(current_path);
+        fs::create_dir(current_path.clone());
+
+        let sumatra_home_directory = SumatraHomeDirectory(current_path.to_owned());
+        return sumatra_home_directory
     }
+    pub fn init_config(&self) -> SumatraHomeDirectoryConfig {
+        let home_dir = &self.0;
+        let mut current_path = home_dir.to_owned();
+        
+        current_path.push(Path::new(CONFIGNAME));
+
+        fs::create_dir(&current_path);
+
+        let final_path = current_path.clone();
+
+        let sumatraconfig = SumatraHomeDirectoryConfig(final_path);
+        return sumatraconfig
+    }
+    pub fn init_keyspace(&self) -> SumatraHomeDirectoryKeyspace {
+        let mut home_dir = &self.0;
+        let mut current_path = home_dir.to_owned();
+        
+        current_path.push(Path::new(KEYSPACE));
+
+        fs::create_dir(&current_path);
+
+        let final_path = current_path.clone();
+
+        let sumatrakeyspace = SumatraHomeDirectoryKeyspace(final_path);
+        return sumatrakeyspace
+    }
+}
+
+impl SumatraHomeDirectoryKeyspace {
+
 }
 
 #[test]
 fn create_file() {
-    SumatraHomeDirectory::init();
+    SumatraHomeDirectory::init_home();
 }
